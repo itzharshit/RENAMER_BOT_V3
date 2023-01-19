cd733a617c0ae03fcfdac8259db92e09de84688f
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-01-19 18:33:02 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from helper.date import add_date
 from helper.database import uploadlimit , usertype,addpre
 ADMIN = int(os.environ.get("ADMIN", 5018650277)
-from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
+from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup, ForceReply)
 
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["warn"]))
