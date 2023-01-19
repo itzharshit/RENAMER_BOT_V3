66b42a583a7d0c246d4600ede06c60fdbab62c2f
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-01-19 14:34:01 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -2,28 +2,28 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from helper.database import *
 
-@Client.on_message(filters.private & filters.command('set_caption'))
+@Client.on_message(filters.private & filters.command('setcaption'))
 async def add_caption(client, message):
     if len(message.command) == 1:
-       return await message.reply_text("**Give me a caption to set.\n\nExample:- `/set_caption File Name`**")
+       return await message.reply_text("**Please include your captions.\n\nExample:- `/setcaption pyrogrammers`**")
     caption = message.text.split(" ", 1)[1]
     addcaption(int(message.chat.id), caption)
-    await message.reply_text("**Your Caption successfully added ✅**")
+    await message.reply_text("**Captions added ✅**")
 
-@Client.on_message(filters.private & filters.command('del_caption'))
+@Client.on_message(filters.private & filters.command('remcaption'))
 async def delete_caption(client, message): 
     caption = find(int(message.chat.id))[1]
     if not caption:
-        await message.reply_text("**You dont have any custom caption**")
+        await message.reply_text("**No captions available to remove.**")
         return
     delcaption(int(message.chat.id))
-    await message.reply_text("**Your caption successfully deleted ✅**")
+    await message.reply_text("**Captions removed ✅**")
                                        
-@Client.on_message(filters.private & filters.command('see_caption'))
+@Client.on_message(filters.private & filters.command('caption'))
 async def see_caption(client, message): 
     caption = find(int(message.chat.id))[1]
     if caption:
-       await message.reply_text(f"<b><u>Your Caption:</b></u>\n\n`{caption}`")
+       await message.reply_text(f"<b><u>Available captions:</b></u>\n\n`{caption}`")
     else:
-       await message.reply_text("**You dont have any custom caption**")
+       await message.reply_text("**No captions available.**")
           
