461227224273255691db8036a8bd50b9b1233543
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-01-19 19:26:20 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,10 +1,9 @@
 import os
+
 from pyrogram import Client, filters
 from helper.date import add_date
 from helper.database import uploadlimit , usertype,addpre
 ADMIN = int(os.environ.get("ADMIN", 5018650277)
-from pyrogram.types import*
-
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["warn"]))
 async def warn(c, m):
