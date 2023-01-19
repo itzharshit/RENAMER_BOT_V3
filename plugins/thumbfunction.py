9a1c8ae6d34a8c9ef19bda38e3feea67aa433219
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-01-19 17:13:42 +0530

    Update thumbfunction.py

--- plugins/thumbfunction.py
+++ plugins/thumbfunction.py
@@ -1,24 +1,24 @@
 from pyrogram import Client, filters
 from helper.database import find, delthumb, addthumb
 
-@Client.on_message(filters.private & filters.command(['viewthumb']))
+@Client.on_message(filters.private & filters.command(['Showthumb']))
 async def viewthumb(client,message):
 		print(message.chat.id)
 		thumb = find(int(message.chat.id))[0]
 		if thumb :
 			await client.send_photo(message.chat.id,photo =f"{thumb}")
 		else:
-			await message.reply_text("**You don't have any custom thumbnail**")
+			await message.reply_text("**No thumbnail found.**")
 	
 	
-@Client.on_message(filters.private & filters.command(['delthumb']))
+@Client.on_message(filters.private & filters.command(['remthumb']))
 async def removethumb(client,message):
 	delthumb(int(message.chat.id))
-	await message.reply_text("**Custom thumbnail deleted successfully**")
+	await message.reply_text("**Successfully Removed.**")
 
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client,message):
 	file_id = str(message.photo.file_id)
 	addthumb(message.chat.id , file_id)
-	await message.reply_text("**Custom thumbnail saved successfully** ✅")
+	await message.reply_text("**Thumbnail saved.** ✅")
 	
