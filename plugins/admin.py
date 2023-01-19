18d024ac9a8ab4b1475f736a9b60b6024bd1ad4f
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-01-19 19:39:35 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -21,28 +21,28 @@ async def warn(c, m):
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["addpremium"]))
 async def buypremium(bot, message):
 	await message.reply_text("Select Plan.........",quote=True,reply_markup=InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("VIP 1",callback_data = "vip1"), 
-        			InlineKeyboardButton("VIP 2",callback_data = "vip2") ]]))
+        			InlineKeyboardButton("Plan 1",callback_data = "vip1"), 
+        			InlineKeyboardButton("Plan 2",callback_data = "vip2") ]]))
         			
 
 @Client.on_callback_query(filters.regex('vip1'))
 async def vip1(bot,update):
 	id = update.message.reply_to_message.text.split("/addpremium")
 	user_id = id[1].replace(" ", "")
-	inlimit  = 10737418240
-	uploadlimit(int(user_id),10737418240)
+	inlimit  = 5740743977
+	uploadlimit(int(user_id),5740743977)
 	usertype(int(user_id),"VIP1")
 	addpre(int(user_id))
 	await update.message.edit("Added successfully To Premium Upload limit 10 GB")
-	await bot.send_message(user_id,"Hey Ur Upgraded To VIP 1 check your plan here /myplan")
+	await bot.send_message(user_id,"Thanks for subscribing to our premium plan, hit /activeplan")
 
 @Client.on_callback_query(filters.regex('vip2'))
 async def vip2(bot,update):
 	id = update.message.reply_to_message.text.split("/addpremium")
 	user_id = id[1].replace(" ", "")
-	inlimit  = 53687091200
-	uploadlimit(int(user_id),53687091200)
+	inlimit  = 5740743977
+	uploadlimit(int(user_id),5740743977)
 	usertype(int(user_id),"VIP2")
 	addpre(int(user_id))
 	await update.message.edit("Added successfully To Premium Upload limit 50 GB")
-	await bot.send_message(user_id,"Hey Ur Upgraded To VIP 2 check your plan here /myplan")
+	await bot.send_message(user_id,"Thanks for subscribing to our premium plan, hit /activeplan")
